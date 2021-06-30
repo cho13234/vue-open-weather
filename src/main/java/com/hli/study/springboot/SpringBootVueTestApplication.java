32d527e9c0a4ab4716b81f902c6f9320cb988f8c
cho13 <cho13@DESKTOP-RG56V8D>
2021-07-01 00:04:02 +0900

    UPLOAD CICD SETTINGS

--- src/main/java/com/hli/study/springboot/SpringBootVueTestApplication.java
+++ src/main/java/com/hli/study/springboot/SpringBootVueTestApplication.java
@@ -2,12 +2,20 @@ package com.hli.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootVueTestApplication {
+	
+//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//			+ "classpath:application.yml,"
+//			+ "classpath:application-real.yml";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVueTestApplication.class, args);
+//		new SpringApplicationBuilder(SpringBootVueTestApplication.class)
+//			.properties(APPLICATION_LOCATIONS)
+//			.run(args);
 	}
 
 }
---
+++ src/main/java/com/hli/study/springboot/common/ProfileController.java
@@ -0,0 +1,29 @@
+package com.hli.study.springboot.common;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@Configuration
+public class ProfileController {
+	
+	private Environment env;
+	
+	public ProfileController(Environment env) {
+		this.env = env;
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		return Arrays.stream(env.getActiveProfiles())
+				.findFirst()
+				.orElse("");
+	}
+}
