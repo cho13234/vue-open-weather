48c87185a2233c1c95bdebc4d9a7a589394eb69a
cho13 <cho13@DESKTOP-RG56V8D>
2021-07-01 00:41:47 +0900

    UPDATE DEPLOY SETTINGS

---
+++ scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+REPOSITORY = /home/ec2-user/app
+PROJECT_NAME = springboot-webservice
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/deploy/target/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+# 실행 중이면 종료하기 위해서 현재 수행 중인 프로세스id를 찾습니다.
+# springboot-webservice으로 된 다른 프로그램들이 있을 수 있어 springboot-webservice된 jar 프로세스를 찾은 뒤 id를 찾습니다(awk '{print $1}').
+CURRENT_PID = $(pgrep -fl springboot-webservice | grep java | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME = $(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR name: JAR_NAME"
+echo "> $JAR_NAME에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # nohup실행 시 CodeDeploy는 무한 대기 합니다. 이 이슈를 해결하기 위해 nohub.out파일을 표준 입출력용으로 별도로 사용합니다. 이렇게 하지 않으면 nohup.out파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력됩니다. nohub이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 이렇게 해야합니다.
